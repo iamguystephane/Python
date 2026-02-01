5585a7479946d1dd311cfbba5b5334d08c4d0eb6
iamguystephane <gstephane138@gmail.com>
2026-02-01 17:02:36 +0100

    first commit learning python. I learned datatypes, lists and dictionaries

---
+++ main.py
@@ -0,0 +1,14 @@
+# Lists in python
+users = ["John", "Mary"];
+
+for user in users:
+    print(user);
+
+# Dictionaries in python
+user_ages = {
+    "John": 30,
+    "Mary": 25
+}
+
+for user, age in user_ages.items():
+    print(f"{user} is {age} years old");
