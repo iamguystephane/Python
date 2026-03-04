e2d01c1da53fe6b76d3e9f721af76f888c5ecc29
iamguystephane <gstephane138@gmail.com>
2026-03-04 23:53:33 +0100

    program working. Fixed all bugs

--- guessing_game.py
+++ guessing_game.py
@@ -4,23 +4,31 @@ TOTAL_GUESSES = 5
 
 def ask_guess():
     guess = input("Enter any number between 1 to 100: ")
-    return guess
+    return int(guess)
 
 
 def check_guess(target):
     guess_count = 0
     guess = ask_guess()
     while (guess_count < TOTAL_GUESSES):
+        guess_count += 1
         if guess == target:
-            return 1
+            return guess_count
         if guess < target:
             print("Too Low!")
-            guess_count += 1
         if guess > target:
             print("Too high!")
-            guess_count += 1
         guess = ask_guess()
+    return 0
 
 
 def main():
-    check_guess()
+    result = check_guess(44)
+    if result == 0:
+        print("You ran out of guesses. Run the program to try again.")
+        return
+    print(f"You guessed the number correctly in {result} number of guesses.")
+    print("Well done.")
+
+
+main()
